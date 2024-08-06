98edca8588bcc808da255583b67431e14511e20b
Damyan Pepper <damyanp@microsoft.com>
2024-08-05 21:23:32 -0700

    get milestones

--- tools/hlslpm/github.py
+++ tools/hlslpm/github.py
@@ -1,6 +1,8 @@
+from dataclasses import dataclass
 import os
 import sys
 import requests
+from datetime import datetime
 
 
 class GH:
@@ -17,6 +19,48 @@ class GH:
 
         return response.json()
 
+@dataclass
+class ItemSummary:
+    item_id: str
+    item_updatedAt: datetime
+    category: str
+    issue_id: str
+    issue_updatedAt: datetime
+
+def maybe_get(d:dict, *args):
+    for a in args:
+        if d != None and type(d) == dict:
+            d = d.get(a, None)
+        else:
+            break
+    return d
+
+def to_datetime(str):
+    if not str:
+        return None
+    return datetime.strptime(str, '%Y-%m-%dT%H:%M:%SZ')
+
+
+def project_items_summary(gh: GH):
+    query = read_file("gql/project_item_summary.gql")
+
+    pageInfo = { 'endCursor': None, 'hasNextPage': True }
+
+    while pageInfo['hasNextPage']:
+        response = gh.graphql(query, {'after': pageInfo['endCursor']})
+
+        items = maybe_get(response, 'data', 'organization', 'projectV2', 'items')
+        for node in items['nodes']:
+            yield ItemSummary(
+                item_id=node['id'],
+                item_updatedAt=to_datetime(maybe_get(node, 'updatedAt')),
+                category=maybe_get(node, 'fieldValueByName', 'name'),
+                issue_id=maybe_get(node, 'content', 'id'),
+                issue_updatedAt=to_datetime(maybe_get(node,'content','updatedAt')))
+            
+        pageInfo = maybe_get(items, 'pageInfo')
+
+
 
 def get_pat():
     try:
@@ -32,7 +76,15 @@ def read_file(filename):
         return f.read()
 
 
-if __name__ == '__main__':
+if __name__ == '__main__':    
     gh = GH()
 
-    print(gh.graphql(read_file("gql/project_item_summary.gql")))
+    items_summary = [i for i in project_items_summary(gh)]
+    print(f"{len(items_summary)} items")
+
+    milestones = [i for i in items_summary if i.category == "Project Milestone"]
+
+    for milestone in milestones:
+        print(milestone)
+
+    # print(gh.graphql(read_file("gql/project_item_summary.gql")))
