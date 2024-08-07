1ce45b70d86aece7afeb094d9d92f85e7461797d
Damyan Pepper <damyanp@microsoft.com>
2024-08-06 18:00:40 -0700

    Can inject test issues

--- tools/hlslpm/hlslpm.py
+++ tools/hlslpm/hlslpm.py
@@ -14,19 +14,45 @@ class Issues:
 
         def is_interesting(i: Issue):
             return i.category in [Category.ProjectMilestone, Category.Workstream]
-        
-        self.all_issues = dict([(i.issue_resourcePath, i) for i in gh.project_items_summary()])
 
-        interesting = [i for i in self.all_issues.values() if is_interesting(i)]
+        self.all_issues = dict([(i.issue_resourcePath, i)
+                               for i in gh.project_items_summary()])
+
+        interesting = [i for i in self.all_issues.values()
+                       if is_interesting(i)]
         interesting = [i for i in gh.get_issues(interesting)]
 
-        self.milestones = [i for i in interesting if i.category == Category.ProjectMilestone]
-        self.workstreams = [i for i in interesting if i.category == Category.Workstream]
-
+        self.milestones = [
+            i for i in interesting if i.category == Category.ProjectMilestone]
+        self.workstreams = [
+            i for i in interesting if i.category == Category.Workstream]
 
 
 if __name__ == '__main__':
-    issues = Issues(123)
+    # issues = Issues(github.GH())
+
+    def issue(i: int, category: Category, body: List[str]):
+        newIssue = Issue(issue_id=f"id{i}")
+        newIssue.category = category
+        newIssue.issue_resourcePath = f"test/{i}"
+        newIssue.body = "\n".join(body)
+        return newIssue
+
+    test_issues = [issue(1, Category.ProjectMilestone, ["## Workstreams", "### WorkstreamA (#2)"]),
+                   issue(2, Category.Workstream, [
+                         "## Milestones", "### Some Milestone (#1)", "foo"])
+                   ]
+    
+    class GHStub:
+        def project_items_summary(self):
+            for i in test_issues:
+                yield i
+
+        def get_issues(self, issues):
+            for i in issues:
+                yield i
+
+    issues = Issues(GHStub())
 
     print(issues.milestones)
     print(issues.workstreams)
